2d662944fcd53a988b766afd3c789ed0ac9d4dde
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2021-10-05 16:11:10 -0400

    Updated python_repos.py  to include plotly bar chart

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,9 @@
+# Interactive Bar Chart using data from GitHub API
+# Using requests library and plotly
 import requests
+from plotly.graph_objs import Bar
+from plotly import offline
+
 
 #Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -8,22 +13,49 @@ print(f"Status code:{r.status_code}")
 
 #Store API in response variable
 response_dict = r.json()
-print(f"Total respositories: {response_dict['total_count']}")
-
-#Explore information about the repositories
+# print(f"Total respositories: {response_dict['total_count']}")
+# Explore information about the repositories
 repo_dicts = response_dict['items']
-print(f"Respositories returned: {len(repo_dicts)}")
+# print(f"Respositories returned: {len(repo_dicts)}")
+# repo_dict = repo_dicts[0] #placed here when i only wanted information from one repo
+repo_names, stars = [],[]
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+# Make bar chart
+data = [{
+'type': 'bar',
+'x':repo_names,
+'y':stars,
+}]
 
-#Examine the first repository
-repo_dict = repo_dicts[0]
-print("\nSelected information about first respository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository:{repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+my_layout = {
+    'title':'Most-Starred Python Projects on GitHub',
+    'xaxis':{'title': 'Repository'},
+    'yaxis':{'title': 'Stars'},
+}
+
+fig = {'data':data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
+#Examine the first repository  returned from the printed responses
+# print("\nSelected information about first respository:")
+# Getting information from more than just top repo
+# for repo_dict in repo_dicts:
+#     # Name
+#     print(f"Name: {repo_dict['name']}")
+#     # Owner of the top repo
+#     print(f"Owner: {repo_dict['owner']['login']}")
+#     # Number of times people have stared the top repo
+#     print(f"Stars: {repo_dict['stargazers_count']}")
+#     # URL
+#     print(f"Repository:{repo_dict['html_url']}")
+#     # Create Date
+#     print(f"Created: {repo_dict['created_at']}")
+#     # Last Date the top repo was updated
+#     print(f"Updated: {repo_dict['updated_at']}")
+#     # Description
+#     print(f"Description: {repo_dict['description']}")
+
 
 # print(f"\nKeys:{len(repo_dict)}")
 # for key in sorted(repo_dict.keys()):
--- readme.md
+++ readme.md
@@ -1,5 +1,28 @@
-# API Called
-Data visualization where data is pulled from GitHub's API
+# API CALLED
+Interactive Bar Chart where data is pulled from GitHub's API.
+Created Bar Chart html document using the python library Plotly, which can be sent within an email showing the interactive Bar Graph.
+Pulled data from GitHub API using the python library requests.
 
-## Requirements
-Python 3.6
+## REQUIREMENTS
+- python 3.7
+- virtualenv
+- requests
+
+## RUNNING DOCUMENTATION LOCALLY:
+1. Create a VM; here are the steps [Creating_A_VM](https://github.com/jesspencer/Good-Grub/blob/master/Creating_A_VM.md)
+2. Run the virtual environment
+3. Clone the project https://github.com/jesspencer/calling_api
+4. Start the code to produce the latest Bar Chart visualization from GitHub's API. The results will show as a html in your browser.
+
+## RESULTS
+![alt text](https://github.com/jesspencer/calling_api/blob/master/python_repos.png?raw=true)
+
+
+## BUGS AND FEATURE REQUESTS
+Have a bug or a feature request? Please open an [issue](https://github.com/jesspencer/calling_api/issues/new).
+
+## CREATOR
+**Jessica Spencer**
+- github.com/jesspencer
+- twitter.com/js13142
+- linkedin.com/in/spenje/
